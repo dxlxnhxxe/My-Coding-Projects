469498fc952e324011ccf707207e50f30e7367e1
dxlxnhxxe <dilanhaye@gmail.com>
2024-04-21 22:26:01 +0200

    Updated

--- Javascript/ConnectFour/index.js
+++ Javascript/ConnectFour/index.js
@@ -1,6 +1,30 @@
-document.addEventListener('DomContentLoaded', () => 
-{
+document.addEventListener('DomContentLoaded', () => {
     const squares = document.querySelectorAll('.grid.div')
     const result = document.querySelector('#result')
-    const displayCurrentPlayer = document.querySelector
+    const displayCurrentPlayer = document.querySelector('#Current-Player')
+    let currentPlayer = 1
+
+    function checkBoard() {
+
+    }
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].onclick = () => {
+        //if the square below your current square is taken you cam go on top of it
+        if (squares[i + 7].classList.contains('taken')) {
+            if (currentPlayer == 1) {
+                squares[i].classList.add('taken')
+                squares[i].classList.add('player-one')
+                currentPlayer = 2
+                displayCurrentPlayer.innerHTML = currentPlayer
+            } else if (currentPlayer == 2) {
+                squares[i].classList.add('taken')
+                squares[i].classList.add('player-one')
+                currentPlayer = 1
+                displayCurrentPlayer.innerHTML = currentPlayer
+                
+            } 
+        } else alert("can't go here")
+        checkBoard()
+        }
+    }
 })
