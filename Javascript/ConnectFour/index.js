23cb997cf89b11e9453de1b0dc41ac0a533fdc06
dxlxnhxxe <dilanhaye@gmail.com>
2024-04-21 20:46:58 +0200

    Practicing Javascript
    Practicing Javascript with 7 games

---
+++ Javascript/ConnectFour/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DomContentLoaded', () => 
+{
+    const squares = document.querySelectorAll('.grid.div')
+    const result = document.querySelector('#result')
+    const displayCurrentPlayer = document.querySelector
+})
---
+++ Javascript/Rock paper scissors/app.js
@@ -0,0 +1,44 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1 // possibleChoices.length
+    console.log(randomNumber)
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'paper'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'scissors'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'its a draw!'
+    }
+    else if ((computerChoice === 'rock' && userChoice === "paper") ||
+             (computerChoice === 'scissors' && userChoice === "rock") ||
+             (computerChoice === 'paper' && userChoice === "scissors")) {
+        result = 'you win!'
+    }
+    else {
+        result = 'you lose!'
+    }
+    resultDisplay.innerHTML = result
+}
