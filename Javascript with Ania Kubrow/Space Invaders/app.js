7b28be4697bc8116bcbd225d13de220355edaa9b
dxlxnhxxe <dilanhaye@gmail.com>
2024-04-22 16:40:37 +0200

    Update

--- Javascript with Ania Kubrow/Space Invaders/app.js
+++ Javascript with Ania Kubrow/Space Invaders/app.js
@@ -1,38 +1,40 @@
 const grid = document.querySelector('.grid')
 let currentShooterIndex = 202
 let width = 15
+let direction = 1
 
 for (let i = 0; i < 225; i++) {
-    const square = document.createElementNS('div')
+    const square = document.createElement('div')
     grid.appendChild(square)
 }
+
+const squares = Array.from(document.querySelectorAll('.grid div'))
 
 const alienInvaders = 
     [
     0,1,2,3,4,5,6,7,8,9,
-    10,11,12, 13, 14, 15, 16, 17, 18, 19,
-    20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-    30, 31, 32, 33, 34, 35 ,36, 37, 38, 39
+    15,16,17,18,19,20,21,22,23,24,
+    30,31,32,33,34,35,36,37,38,39
     ]
 
 function draw() {
     for (let i = 0; i < alienInvaders.length; i++) {
-        squares[alienInvaders[i]].classList.add('invader')
+        squares[alienInvaders[i]].classList.add('invader');
+        }
     }
-}
 
 draw()
 
-function remove () {
+function remove() {
     for (let i = 0; i < alienInvaders.length; i++) {
-        squares[alienInvaders[i]].classList.remove('invader')
+        squares[alienInvaders[i]].classList.remove('invader');
+        }
     }
-}
 
 squares[currentShooterIndex].classList.add('shooter')
 
 function moveShooter(e) {
-    squares[currentShooterIndex].classList.add('shooter')
+    squares[currentShooterIndex].classList.remove ('shooter')
     switch(e.key) {
         case 'ArrowLeft':
             if (currentShooterIndex % width !== 0) currentShooterIndex -= 1
@@ -50,11 +52,12 @@ function moveInvaders() {
     const leftEdge = alienInvaders[0] % width === 0
     const rightEdge = alienInvaders[alienInvaders.length - 1] % width === width - 1
     remove()
+
     for (let i = 0; i < alienInvaders.length; i++) {
-        alienInvaders[i] += 1
+        alienInvaders[i] += direction
     }
 
     draw()
 }
 
-setInterval(moveInvaders, 50)
+setInterval(moveInvaders, 500)
