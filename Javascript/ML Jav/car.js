61e7c2bcd65c2895e75a45b8ab7cbd44fde5e2f9
dxlxnhxxe <dilanhaye@gmail.com>
2024-05-21 23:52:46 +0100

    Update

--- Javascript/ML Jav/car.js
+++ Javascript/ML Jav/car.js
@@ -37,28 +37,29 @@ class Car {
         if (this.speed < 0) {
             this.speed += this.friction;
         }
-        if(Math.abs(this.speed)<this.friction){
-            this.speed=0;
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
         }
 
-        if(this.speed!=0){
-            const flip=this.speed>0?1:-1;
-            if(this.controls.left){
-                this.angle+=0.03*flip;
+        if (this.speed != 0) {
+            const flip = this.speed > 0 ? 1 : -1;
+            if (this.controls.left) {
+                this.angle -= 0.03 * flip;
             }
-            if(this.controls.right){
-                this.angle-=0.03*flip;
+            if (this.controls.right) {
+                this.angle += 0.03 * flip;
             }
         }
+
         this.x -= Math.sin(this.angle) * this.speed;
         this.y -= Math.cos(this.angle) * this.speed;
     }
+
     draw(ctx) {
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle);
 
-        ctx.beginPath();
         ctx.fillStyle = 'red';
         ctx.fillRect(
             -this.width / 2,
@@ -66,6 +67,7 @@ class Car {
             this.width,
             this.height
         );
+
         ctx.restore();
     }
 }
--- Javascript/ML Jav/road.js
+++ Javascript/ML Jav/road.js
@@ -1,5 +1,5 @@
 class Road {
-    constructor(x, y, width, laneCount=3) {
+    constructor(x, y, width, laneCount = 3) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -11,6 +11,12 @@ class Road {
         const infinity = 1000000;
         this.top = -infinity;
         this.bottom = infinity;
+
+        const laneWidth = this.width / this.laneCount;
+        this.laneCenters = [];
+        for (let i = 0; i <= laneCount; i++) {
+            this.laneCenters.push(this.left + i * laneWidth);
+        }
     }
 
     draw(ctx) {
@@ -28,5 +34,17 @@ class Road {
         ctx.moveTo(this.right, this.top);
         ctx.lineTo(this.right, this.bottom);
         ctx.stroke();
+
+        // Draw lane markings
+        ctx.lineWidth = 2;
+        ctx.setLineDash([20, 20]);
+        for (let i = 1; i < this.laneCount; i++) {
+            const x = this.left + i * (this.width / this.laneCount);
+            ctx.beginPath();
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.bottom);
+            ctx.stroke();
+        }
+        ctx.setLineDash([]);
     }
 }
